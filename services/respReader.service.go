7bd8259903ee0a64d86f2f3505287cf86f6b2bae
IbrahimItani <i.itani2002@gmail.com>
2025-07-20 17:26:16 +0300

    reader<>re-engineered the resp reader

--- services/respReader.service.go
+++ services/respReader.service.go
@@ -4,27 +4,121 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"keeva-db/utils"
-	"os"
 	"strconv"
-	"strings"
 )
 
-func RESPReader (input string) {
-	reader := bufio.NewReader(strings.NewReader(input))
-	byte,_ := reader.ReadByte()
-	if byte != '$'{
-		fmt.Println("Invalid type, only bulk strings accepted")
-		os.Exit(1)
-	}
-	size,_ := reader.ReadByte()
-	strSize,_ := strconv.ParseInt(string(size),10,64)
-	utils.ConsumeUnecessaryBytes(2,reader)
-	bulkString := make([]byte, strSize)
-	reader.Read(bulkString)
-	fmt.Println(string(bulkString))
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Value struct {
+	typ   string
+	str   string
+	num   int
+	bulk  string
+	array []Value
 }
 
-func NewResp(rd io.Reader) *utils.Resp {
-	return &utils.Resp{reader: bufio.NewReader(rd)}
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+	return line[:len(line)-2], n, nil
+}
+
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+	i64, err := strconv.ParseInt(string(line), 10, 64)
+	if err != nil {
+		return 0, n, err
+	}
+	return int(i64), n, nil
+}
+
+func (r *Resp) Read() (Value, error) {
+	_type, err := r.reader.ReadByte()
+
+	if err != nil {
+		return Value{}, err
+	}
+
+	switch _type {
+	case ARRAY:
+		return r.readArray()
+	case BULK:
+		return r.readBulk()
+	default:
+		fmt.Printf("Unknown type: %v", string(_type))
+		return Value{}, nil
+	}
+}
+
+func (r *Resp) readArray() (Value, error) {
+	v := Value{}
+	v.typ = "array"
+
+	// read length of array
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err
+	}
+
+	// foreach line, parse and read the value
+	v.array = make([]Value, 0)
+	for i := 0; i < len; i++ {
+		val, err := r.Read()
+		if err != nil {
+			return v, err
+		}
+
+		// append parsed value to array
+		v.array = append(v.array, val)
+	}
+
+	return v, nil
+}
+
+func (r *Resp) readBulk() (Value, error) {
+	v := Value{}
+
+	v.typ = "bulk"
+
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err
+	}
+
+	bulk := make([]byte, len)
+
+	r.reader.Read(bulk)
+
+	v.bulk = string(bulk)
+
+	// Read the trailing CRLF
+	r.readLine()
+
+	return v, nil
 }
